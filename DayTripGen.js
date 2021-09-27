e25abcefb2c0aaf8cf4323a05027c07231279966
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-09-27 05:15:12 -0400

    second commit

--- DayTripGen.js
+++ DayTripGen.js
@@ -8,19 +8,80 @@ let userDestinations = prompt("Choose a number between 0 and 3")
 
 //function getDestination(userDestinations)
     
-   for(let index = 0; index < destinations.length; index++){
+   for(let index = 0; index , destinations.length;){ 
        if (userDestinations[index] === 0) {
-           console.log('You will be traveling to Cleveland')
+           console.log ('You will be traveling to Cleveland')
        }
+       if (userDestinations[index] === 1) {
+           console.log ('You will be traveling to Cincinnati')
+       }
+       if (userDestinations[index] === 2) {
+           console.log ('You will be traveling to Columbus')
+       }
+       if (userDestinations[index] ===3) {
+           console.log (' You will be traveling to Sandusky')
+        }
    } 
         
 
+let restaurants = ['Pizza', 'Street Tacos', 'Hibachi', 'Mongolian'];
+console.log(restaurants)
 
-//let restaurants = ['Pizza', 'Street Tacos', 'Hibachi', 'Mongolian'];
-//console.log(restaurants)
+let userRestaurants = prompt("Choose another number between 0 and 3")
 
-//let transportation = ['Uber', 'Limo', 'RV', 'Private Jet'];
-//console.log(transportation)
+for(let index = 0; index , restaurants.length;){ 
+    if (userRestaurants[index] === 0) {
+        console.log ('You will be having Pizza')
+    }
+    if (userRestaurants[index] === 1) {
+        console.log ('You will be having Street Tacos')
+    }
+    if (userRestaurants[index] === 2) {
+        console.log ('You will be having Hibachi')
+    }
+    if (userRestaurants[index] ===3) {
+        console.log (' You will be having Mongolian')
+     }
+} 
 
-//let entertainment = ['local concert', 'local sporting event', 'bar/arcade', 'Oktoberfest'];
-//console.log(entertainment)
+let transportation = ['Uber', 'Limo', 'RV', 'Private Jet'];
+console.log(transportation)
+
+let userTransportation = prompt("Choose another number between 0 and 3")
+
+for(let index = 0; index , transportation.length;){ 
+    if (userTransportation[index] === 0) {
+        console.log ('You will be taking an Uber')
+    }
+    if (userTransportation[index] === 1) {
+        console.log ('You will be taking an Limo')
+    }
+    if (userTransportation[index] === 2) {
+        console.log ('You will be taking an RV')
+    }
+    if (userTransportation[index] ===3) {
+        console.log (' You will be taking an Private Jet')
+     }
+} 
+
+let entertainment = ['local concert', 'local sporting event', 'bar/arcade', 'Oktoberfest'];
+console.log(entertainment)
+
+let userEntertainment = prompt("Choose another number between 0 and 3")
+
+for(let index = 0; index , entertainment.length;){ 
+    if (userEntertainment[index] === 0) {
+        console.log ('You will be going to a local concert')
+    }
+    if (userEntertainment[index] === 1) {
+        console.log ('You will be going to a local sporting event')
+    }
+    if (userEntertainment[index] === 2) {
+        console.log ('You will be going to a bar/arcade')
+    }
+    if (userEntertainment[index] ===3) {
+        console.log (' You will be going to an Oktoberfest')
+     }
+} 
+
+//Math.random()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # bigRedRocketship
 
 Added four arrays with each of their unique elements,
+
+Added prompts for each segiment (destinations, restaurants, transportation, entertainment)
